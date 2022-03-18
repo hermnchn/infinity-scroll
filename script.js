bc32b83fe4e41548da02cc342536a11635b8cbd9
Herman <hermnchn@gmail.com>
2022-03-18 00:32:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const imageContainer = doucment.getElementById('image-container');
+const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 20;
 const apiKey = 'ztwGKXCCnXKMSDt-1u_tXIzNZ3XF6mIYALdeLXx6FVA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
     for (const key in attributes) {
-        element.setAttribute(key, attributes[key])
+        element.setAttribute(key, attributes[key]);
     }
 }
 
@@ -20,18 +20,13 @@ function displayPhotos() {
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
-        const item = document.createdElement('a');
-        // item.setAttribute('href', photo.links.html);
-        // item.setAttribute('target', '_blank');
+        const item = document.createElement('a');
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank',
-        })
+        });
         // Create <img> for photo
         const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular);
-        // img.setAttribute('alt', photo.alt_description);
-        // img.setAttribute('title', photo.alt_description);
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
@@ -53,6 +48,14 @@ async function getPhotos() {
     // Catch Error Here
     }
 }
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 // On Load
 getPhotos();
