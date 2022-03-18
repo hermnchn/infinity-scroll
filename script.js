00340ef5ecef79cd60bfdf65b8b944c9003858a8
Herman <hermnchn@gmail.com>
2022-03-17 23:07:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const imageContainer = doucment.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let photosArray = [];
 
 // Unsplash API
 const count = 10;
 const apiKey = "ztwGKXCCnXKMSDt-1u_tXIzNZ3XF6mIYALdeLXx6FVA";
 const apiUrl = "https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}";
 
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createdElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        // Create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+    });
+}
+
 // Get photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
     // Catch Error Here
     }
