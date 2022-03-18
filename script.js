5584b72b6e503507c1692939357dcd3f896786f7
Herman <hermnchn@gmail.com>
2022-03-18 00:35:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 const count = 20;
 const apiKey = 'ztwGKXCCnXKMSDt-1u_tXIzNZ3XF6mIYALdeLXx6FVA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
